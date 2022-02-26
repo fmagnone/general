2350a7bc25f579355da74906e005f0220e474e23
fmagnone <fmagnone@gmail.com>
2022-02-26 12:11:26 +0100

    resizeVanillaJS updates

--- resizeVanillaJS/src/scripts.js
+++ resizeVanillaJS/src/scripts.js
@@ -5,7 +5,7 @@
 // Elements
 const imagesDiv = document.querySelector("#images");
 const fileInput = document.querySelector("#upload");
-const updateBtn = document.querySelector("#update");
+//const updateBtn = document.querySelector("#update");
 const slider = document.getElementById("myRange");
 imagesDiv.style.visibility = "hidden";
 
@@ -45,17 +45,10 @@ slider.oninput = function() {
   resizeImage();
 };
 
-updateBtn.onclick = function(){
-  resizeImage();
-};
+//updateBtn.onclick = function(){
+//  resizeImage();
+//};
 
-//var output = document.getElementById("demo");
-//output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-//slider.oninput = function() {
-  //output.innerHTML = this.value;
-//}
 
 // Resizer function
 function resizeImage() {
