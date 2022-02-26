92273e7d50f42e6ea1c24d9c6e77bdf3d67d2d12
fmagnone <fmagnone@gmail.com>
2022-02-26 12:22:25 +0100

    resizeVanillaJS updates

--- resizeVanillaJS/src/scripts.js
+++ resizeVanillaJS/src/scripts.js
@@ -1,33 +1,26 @@
 //
-//
-//
 
 // Elements
 const imagesDiv = document.querySelector("#images");
 const fileInput = document.querySelector("#upload");
-//const updateBtn = document.querySelector("#update");
-const slider = document.getElementById("myRange");
+var imageToResize = document.querySelector("#imgToResize");
+var resizedImage = document.querySelector("#resizedImage");
+var slider = document.getElementById("myRange");
+
 imagesDiv.style.visibility = "hidden";
 
 // Global Variables
 var resizingFactor = 0.5;
-var imageToResize;
-var resizedImage;
 
 // Listeners
 fileInput.addEventListener("change", async (e) => {
   const [file] = fileInput.files;
 
-  // displaying the uploaded image
-  imageToResize = document.querySelector("#imgToResize");
   imageToResize.src = await fileToDataUri(file);
 
   // resizing the image and displaying it
-  resizedImage = document.querySelector("#resizedImage");
-  resizeImage();
-
-  // making the div containing the image visible
   imagesDiv.style.visibility = "visible";
+  resizeImage();
 });
 
 function fileToDataUri(field) {
@@ -40,20 +33,13 @@ function fileToDataUri(field) {
   });
 }
 
-slider.oninput = function() {
-  resizingFactor = this.value/100;
+slider.oninput = function () {
+  resizingFactor = this.value / 100;
   resizeImage();
 };
 
-//updateBtn.onclick = function(){
-//  resizeImage();
-//};
-
-
 // Resizer function
 function resizeImage() {
-  //console.log(resizingFactor, imgToResize);
-
   var canvas = document.createElement("canvas");
   var context = canvas.getContext("2d");
 
@@ -63,16 +49,21 @@ function resizeImage() {
   var canvasWidth = originalWidth * resizingFactor;
   var canvasHeight = originalHeight * resizingFactor;
 
+  console.log("W__", originalWidth, resizingFactor);
+
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
 
   context.drawImage(
-    imgToResize,
-    0,
-    0,
+    imgToResize, 0, 0,
     originalWidth * resizingFactor,
     originalHeight * resizingFactor
   );
 
+  console.log("W__", canvasWidth);
+
   resizedImage.src = canvas.toDataURL();
+
+  console.log(canvas.toDataURL())
+  console.log(resizedImage)
 }
